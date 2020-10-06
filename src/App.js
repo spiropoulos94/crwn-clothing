69a10e764a43bce7865bfb907e467512e863dff3
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-10-07 00:44:59 +0300

    mexri 10.37 tou video

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
-
-const HatsPage = () => {
-    return (
-        <div>
-            <h1 className="hats">Hats page</h1>
-        </div>
-    );
-};
 
 
 function App() {
@@ -18,7 +11,7 @@ function App() {
         <div>
             <Switch>
                 <Route exact path='/' component={HomePage}/>
-                <Route  path='/hats' component={HatsPage}/>
+                <Route  path='/shop' component={ShopPage}/>
             </Switch>
         </div>
 
