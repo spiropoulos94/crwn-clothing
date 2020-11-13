984f3af155f736a06b7a853a7dda82bcc7cf1234
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-11-13 16:24:03 +0200

    add to cart button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
+
+import CustomButton from "../custom-button/custom-button.component";
+
 const CollectionItem = ({id,name, price, imageUrl}) => {
     return (
         <div className="collection-item">
@@ -9,6 +12,7 @@ const CollectionItem = ({id,name, price, imageUrl}) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton inverted >Add to Cart</CustomButton>
         </div>
     );
 };
