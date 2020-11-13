808bc4ff228c1e55b915d34bc585feaf564d9199
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-11-13 13:47:03 +0200

    added redirect and action types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 
 import {connect} from 'react-redux';
 import {setCurrentUser} from "./redux/user/user.actions";
@@ -21,7 +21,7 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        const {setCurrentUser} = this.props;
+        const {setCurrentUser, currentUser} = this.props;
 
 
         //to onAuthStateChanged pairnei ws argument mia function kai kanei subscribe se aythn etsi wste na thn
@@ -62,7 +62,7 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage}/>
+                    <Route exact path='/signin' render={()=> this.props.currentUser ? (<Redirect to='/' />) :( <SignInAndSignUpPage/>)}/>
                 </Switch>
             </div>
         );
@@ -74,8 +74,12 @@ class App extends React.Component {
 // function called mapDispatchToProps() that receives the dispatch() method and returns callback props
 // that you want to inject into the presentational component.
 
+const mapStateToProps = ({user}) => ({
+    currentUser : user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
