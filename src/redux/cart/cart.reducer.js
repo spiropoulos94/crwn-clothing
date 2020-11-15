a1b43ede249e0b9ef16e7438ad8a9fcaace0132d
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-11-15 23:12:33 +0200

    added functionality to checkout page, there is an error

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from "./cart.utils";
+import {addItemToCart, removeItemFromCart} from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -19,6 +19,19 @@ const INITIAL_STATE = {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+
+        case CartActionTypes.REMOVE_ITEM :
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                //krata opoio item den exei id idio me to selected
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
 
         default :
