60fcfa1fca280734875b50df705521649d1761de
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-11-15 12:53:12 +0200

    added selectors and reselect sthn thesi twn cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import {connect} from "react-redux";
 import CartItem from "../cart-item/cart-item.component";
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 
 
@@ -18,7 +19,7 @@ const CartDropdown = ({cartItems}) => {
 };
 
 const mapStateToProps = state => ({
-    cartItems : state.cart.cartItems
+    cartItems : selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
