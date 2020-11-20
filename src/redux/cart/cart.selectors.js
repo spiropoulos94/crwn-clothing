23dfd38b71cb089571eb809b5b435a9df42eb15e
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2020-11-20 17:17:50 +0200

    added and configured redux-persist

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,7 +2,7 @@ import {createSelector} from "reselect";
 
 const selectCart = state => state.cart
 
-export const selectCartItems = createSelector(
+export const selectCartItems = createSelector(   //because we used createSelector, then our selector is memoized
     [selectCart],
     (cart) => cart.cartItems
 );
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,14 @@
 import {combineReducers} from "redux";
 import {persistReducer} from "redux-persist";
 
-import storage from "redux-persist/lib/storage";
+import storage from "redux-persist/lib/storage"; //this imports the local storage lib. for session storage check
+// redux-persist docs
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
 const persistConfig = {
-    key : "root",
+    key : "root", //dilwnei o poio shmeio tou reducer theloume na kanoume store. Edw ta theloume ola ara root
     storage,
     whitelist:["cart"]
 }
