20bc237b62e9759efd2404839b47c8b128910676
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-02-09 22:55:31 +0200

    small fix for unexpected error

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -15,7 +15,9 @@ export const selectCartHidden = createSelector(
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => {
+      return  accumulatedQuantity + cartItem.quantity, 0
+    })
 )
 
 export const selectCartTotal = createSelector(
