4806350dfeafc5afb2048f0bd9b95448fc23b9b3
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-02-15 23:13:15 +0200

    added test page

---
+++ src/pages/test-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CollectionItem from "../components/collection-item/collection-item.component";
+
+const TestPage = () => {
+    return (
+        <div>
+            i am the test page
+            <CollectionItem/>
+        </div>
+    );
+};
+
+export default TestPage;
