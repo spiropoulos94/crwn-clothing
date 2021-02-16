b3dc0162b8469b14891909d7d27c5995458d7f6b
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-02-16 23:32:31 +0200

    data normalization

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux"
 import { createStructuredSelector} from "reselect";
-import {selectCollections} from "../../redux/shop/shop.selector";
+import {selectCollectionForPreview} from "../../redux/shop/shop.selector";
 
 import "./collections-overview.styles.scss";
 import CollectionPreview from "../collection-preview/collection-preview.component";
@@ -18,7 +18,7 @@ const CollectionsOverview = ({collections}) => {
 
 
 const mapStateToProps = () => createStructuredSelector({
-    collections : selectCollections
+    collections : selectCollectionForPreview
 })
 
 
