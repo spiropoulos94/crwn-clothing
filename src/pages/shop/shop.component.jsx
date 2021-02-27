3a0bff15727c11d8c051b08c52e0d42138e0bac6
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-02-27 17:44:17 +0200

    small improvements

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,16 +5,31 @@ import "./shop.styles.scss";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
+import {firestore} from "../../firebase/firebase.utils";
+
 //prokeimenou na deiksume to route pou eimaste twra xisimopoioume to match.path. esi wste se ayto to route na deixeni to synoliko overview
 
-const ShopPage = ({match}) => {
+class ShopPage extends React.Component {
+    unsubscribeFromSnapshot = null;
+
+    componentDidMount() {
+        const collectionRef = firestore.collection("collections");
+        collectionRef.onSnapshot()
+
+        //todo apo edw suunexise
+    }
+
+    render() {
+        const { match } = this.props;
         return (
             <div className="shop-page">
                 <Route exact path={`${match.path}`} component={CollectionsOverview} />
                 <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
             </div>
         );
+    }
 }
+
 
 
 export default ShopPage;
