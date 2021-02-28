c6caddacddf5e0e30a25ea75865b6843f74e6351
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-02-28 21:02:35 +0200

    added HOC comment

--- src/components/withSpinner/with-spinner.component.jsx
+++ src/components/withSpinner/with-spinner.component.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {SpinnerContainer, SpinnerOverlay} from "./with-spinner.styles";
 
+// To WithSpinner einai mia function pou pairnei ena WrappedComponent kai gyrnaei ena kainourio Component.
+//Sthn dikia mas periptwsh to neo component exei ena isLoading prop kai vasei aytou kanei render eite to spinner
+// eite to wrapped component pou exei parei.
+
 const WithSpinner = (WrappedComponent) => ({isLoading, ...otherProps}) => {  //pairnei to wrapComponent kai gyrnaei ena neo me props apo to wrapped
     return isLoading ? (                                                     //isLoading + ...otherProps erxontai apo to wrappedComponent
         <SpinnerOverlay>
